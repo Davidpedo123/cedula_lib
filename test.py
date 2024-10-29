c42f7504e1408af24f143d5c07051cb23fee2a69
Eduardo Tejada <105448798+Davidpedo123@users.noreply.github.com>
2024-10-29 14:01:17 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,30 +1,30 @@
 import time
-import cedula_lib  # Importa el módulo C compilado
+import cedula_lib 
 
-# Función para benchmarking en C
+
 def benchmark_c(num_cedulas):
-    cedulas = ['40233149323'] * num_cedulas  # Genera una lista de cédulas para procesar
+    cedulas = ['40233149323'] * num_cedulas  
     start_time = time.time()
 
-    # Crear una lista de cadenas a partir de las cédulas
+    
     cedulas_list = [str(cedula) for cedula in cedulas]
 
-    # Llamar a la función formatear_cedula del módulo C
+    
     resultado = cedula_lib.formatear_cedula(cedulas_list)
 
     end_time = time.time()
     return end_time - start_time
 
-# Función para ajustar cédula en Python
+
 def ajustar_cedula(cedula):
     cedula_sin_guion = str(cedula).replace("-", "").replace("_", "")
     if len(cedula_sin_guion) < 11:
         cedula_sin_guion = cedula_sin_guion.zfill(11)
     return "%s-%s-%s" % (cedula_sin_guion[0:3], cedula_sin_guion[3:10], cedula_sin_guion[-1])
 
-# Función para benchmarking en Python
+
 def benchmark_python(num_cedulas):
-    cedulas = ['40233149323'] * num_cedulas  # Genera una lista de cédulas para procesar
+    cedulas = ['40233149323'] * num_cedulas  
     start_time = time.time()
 
     for cedula in cedulas:
@@ -33,11 +33,11 @@ def benchmark_python(num_cedulas):
     end_time = time.time()
     return end_time - start_time
 
-# Número de cédulas para probar
 
-num_cedulas = 1000000  # 1 millón de cédulas
+
+num_cedulas = 1000000  
 print(f'Numero de cedulas: {num_cedulas}')
-# Ejecuta el benchmarking
+
 tiempo_c = benchmark_c(num_cedulas)
 tiempo_python = benchmark_python(num_cedulas)
 
